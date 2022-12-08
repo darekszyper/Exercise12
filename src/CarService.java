98fbb912c3942f72821422a780e92af7e7b63c9b
krzy1976 <krzysztof.wojciech.dziedzic@gmail.com>
2022-12-08 19:08:52 +0100

    attempt1

---
+++ src/CarService.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarService {
+    private List<Car> carList;
+
+    public CarService() {
+        this.carList = new ArrayList<>();
+    }
+
+    public void addCar(Car car) {
+        this.carList.add(car);
+    }
+
+    public boolean deleteCar(Car car) {
+        return carList.remove(car);
+    }
